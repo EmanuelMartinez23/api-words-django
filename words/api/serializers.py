98c07605d24ab739758b11e88ed7e94ba282faf6
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-12-22 15:27:06 -0600

    feat: add two enpoint for list words and word details

--- words/api/serializers.py
+++ words/api/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
+from ..models  import Word
+
+# # validate field language
+# def validate_language(value):
+#     # if language field is not String throw exception
+#     if len(value) != 2 :
+#         raise serializers.ValidationError('Language field is not String')
+
 
 class WordSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    word = serializers.CharField()
-    language = serializers.CharField()
-    theme = serializers.CharField()
+    word = serializers.CharField(required=True)
+    language = serializers.CharField(required=True)
+    theme = serializers.CharField(required=True)
     date_joined = serializers.DateField()
 
+    def create(self, validated_data):
+        return Word.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.word = validated_data.get('word', instance.word)
+        instance.language = validated_data.get('language', instance.language)
+        instance.theme = validated_data.get('theme', instance.theme)
+        instance.date_joined = validated_data.get('date_joined', instance.date_joined)
+        instance.save()
+        return instance
--- words/api/views.py
+++ words/api/views.py
@@ -1,21 +1,63 @@
-from django.core.serializers import serialize
-from django.http import JsonResponse, HttpResponse
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
-
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT, \
+    HTTP_404_NOT_FOUND
 from .serializers import WordSerializer
 from ..models import Word
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def word_list(request):
-    words =  Word.objects.all()
-    serializer  = WordSerializer(words, many=True)
-    return Response(data=serializer.data, status=HTTP_200_OK)
+    if request.method == 'GET':
+        words =  Word.objects.all()
+        serializer  = WordSerializer(words, many=True)
+        return Response(data=serializer.data, status=HTTP_200_OK)
+
+    if request.method == 'POST':
+        serializer = WordSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data = serializer.data, status = HTTP_201_CREATED)
+        else:
+            return Response(data = serializer.errors,status = HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def word_details(request, pk):
-    word = Word.objects.get(pk = pk)
-    serializer = WordSerializer(word)
-    return Response(data = serializer.data, status = HTTP_200_OK)
+    if request.method == 'GET':
+        try:
+            word = Word.objects.get(pk = pk)
+        except Word.DoesNotExist:
+            return Response(
+                "{ error : Word not found }",
+                status=HTTP_404_NOT_FOUND)
+
+        serializer = WordSerializer(word)
+        return Response(data = serializer.data, status = HTTP_200_OK)
+
+    if request.method == 'DELETE':
+        try:
+            word = Word.objects.get(pk = pk)
+        except Word.DoesNotExist:
+            return Response(
+                "{error : Word not found }",
+                status = HTTP_404_NOT_FOUND
+            )
+
+        word.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+    if request.method == 'PUT':
+        try:
+            word = Word.objects.get(pk=pk)
+        except Word.DoesNotExist:
+            return Response("{ error : Word not found }", status = HTTP_404_NOT_FOUND)
+
+        serializer =  WordSerializer(instance=word , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data = serializer.data, status = HTTP_200_OK)
+        else:
+            return Response(data = serializer.errors, status =  HTTP_400_BAD_REQUEST)
