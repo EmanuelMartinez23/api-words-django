522dc34b3d82775710bcd713250b76f0faf0e9a2
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-12-29 03:22:44 -0600

    feat: Views created with the APIView class.

--- words/api/urls.py
+++ words/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from ..api.views import word_list, word_details
+from ..api.views import word_list, word_details, word_listAV, word_detailsAV
 
 urlpatterns = [
-    path('', word_list, name = "word-list" ),
-    path('<int:pk>', word_details, name = "word-details" )
+    # path('', word_list, name = "word-list" ),  # decorator @api_view()
+    # path('<int:pk>', word_details, name = "word-details" ),
+    path('', word_listAV.as_view(), name = "word-list" ), # With APIView Class
+    path('<int:pk>', word_detailsAV.as_view(), name = "word-details" )
 ]
--- words/api/views.py
+++ words/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT, \
     HTTP_404_NOT_FOUND
+from rest_framework.views import APIView
+
 from .serializers import WordSerializer
 from ..models import Word
 
@@ -20,7 +22,6 @@ def word_list(request):
             return Response(data = serializer.data, status = HTTP_201_CREATED)
         else:
             return Response(data = serializer.errors,status = HTTP_400_BAD_REQUEST)
-
 
 
 
@@ -61,3 +62,57 @@ def word_details(request, pk):
             return Response(data = serializer.data, status = HTTP_200_OK)
         else:
             return Response(data = serializer.errors, status =  HTTP_400_BAD_REQUEST)
+
+
+# Views with APIView Class
+class word_listAV(APIView):
+
+    def get(self,request):
+        words = Word.objects.all()
+        serializer = WordSerializer(words, many=True)
+        return Response(data=serializer.data, status=HTTP_200_OK)
+
+    def post(self,request):
+        serializer = WordSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+class word_detailsAV(APIView):
+    def get(self,request,pk):
+        try:
+            word = Word.objects.get(pk=pk)
+        except Word.DoesNotExist:
+            return Response(
+                "{ error : Word not found }",
+                status=HTTP_404_NOT_FOUND)
+
+        serializer = WordSerializer(word)
+        return Response(data=serializer.data, status=HTTP_200_OK)
+
+    def put(self,request, pk):
+        try:
+            word = Word.objects.get(pk=pk)
+        except Word.DoesNotExist:
+            return Response("{ error : Word not found }", status = HTTP_404_NOT_FOUND)
+
+        serializer =  WordSerializer(instance=word , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data = serializer.data, status = HTTP_200_OK)
+        else:
+            return Response(data = serializer.errors, status =  HTTP_400_BAD_REQUEST)
+
+    def delete(self, request,pk):
+        try:
+            word = Word.objects.get(pk = pk)
+        except Word.DoesNotExist:
+            return Response(
+                "{error : Word not found }",
+                status = HTTP_404_NOT_FOUND
+            )
+
+        word.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
