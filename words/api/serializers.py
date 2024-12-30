90f4c9bbc655c819f3d74d259a3190f1bf4be2d0
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-12-30 04:07:17 -0600

    feat: Validation at the level (object,field) implemented in the fields

--- words/api/serializers.py
+++ words/api/serializers.py
@@ -1,17 +1,22 @@
+from datetime import timezone, datetime
+
 from rest_framework import serializers
 from ..models  import Word
 
-# # validate field language
-# def validate_language(value):
-#     # if language field is not String throw exception
-#     if len(value) != 2 :
-#         raise serializers.ValidationError('Language field is not String')
+# Validators
+def available_language(value):
+    # language_list = list(Word.LANGUAGE.keys())
+    language_list = ['en','es','it']
+    # validation of available language
+    print(value)
+    if not (value in language_list):
+        raise serializers.ValidationError("Language is not available")
 
 
 class WordSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     word = serializers.CharField(required=True)
-    language = serializers.CharField(required=True)
+    language = serializers.CharField(required=True, validators= [available_language])
     theme = serializers.CharField(required=True)
     date_joined = serializers.DateField()
 
@@ -25,3 +30,20 @@ class WordSerializer(serializers.Serializer):
         instance.date_joined = validated_data.get('date_joined', instance.date_joined)
         instance.save()
         return instance
+
+    # Fields Validation
+
+    # Field-Level validation
+    # validate language field
+    # def validate_language(self,value):
+    #     # if the length of the language field is not 2, throw an exception
+    #     if len(value) != 2:
+    #         raise serializers.ValidationError('The length of the language field is not 2.')
+
+    # Object-Level validation
+    def validate(self, data):
+        # validation
+        if not ( str.isalpha(str(data['word']))  and  str.isalpha(str(data['theme']))):
+            raise serializers.ValidationError("The fields cannot contain numbers.")
+        return data
+
